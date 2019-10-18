60b3f269ac5c57dacff5457daaded216cd6611d4
nclsgg <nicolasguadagno@gmail.com>
2019-10-18 09:33:51 -0300

    Finished

---
+++ src/pages/Main/index.js
@@ -0,0 +1,115 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable no-throw-literal */
+import React, { Component } from "react";
+import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import api from "../../services/api";
+
+import Container from "../../Components/Container";
+
+import { Form, SubmitButton, List } from "./style";
+
+export default class Main extends Component {
+  state = {
+    newRepo: "",
+    repositories: [],
+    loading: false,
+    error: null
+  };
+
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem("repositories");
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem("repositories", JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value, error: null });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true, error: false });
+
+    try {
+      const { newRepo, repositories } = this.state;
+
+      if (newRepo === "") throw "You must enter a repository.";
+
+      const hasRepo = repositories.find(r => r.name === newRepo);
+
+      if (hasRepo) throw "This repository is already added.";
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: ""
+      });
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { newRepo, loading, repositories, error } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit} error={error}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Details
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
